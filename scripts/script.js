4fa45572ce6636700fbf3464b643c865bef734b9
MarkvHeusden <74104639+MarkvHeusden@users.noreply.github.com>
2020-12-03 00:12:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-// JavaScript Document
+const elFooterLinks = document.querySelectorAll('.links > li');
+
+for (let i = 0; i < elFooterLinks.length; i++) {
+    elFooterLinks[i].addEventListener('click', function() {
+        this.classList.toggle('links_uitgevouwd');
+    })
+}
